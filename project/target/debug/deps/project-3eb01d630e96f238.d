096845753c0abc5a0508beee262089b4ddc546c0
a-scarlett <a6scarlett@gmail.com>
2023-10-24 10:59:25 +0300

    empty project

---
+++ project/target/debug/deps/project-3eb01d630e96f238.d
@@ -0,0 +1,5 @@
+/home/linux/rust/rust_first_application/project/target/debug/deps/project-3eb01d630e96f238: src/main.rs
+
+/home/linux/rust/rust_first_application/project/target/debug/deps/project-3eb01d630e96f238.d: src/main.rs
+
+src/main.rs:
---
+++ project/target/debug/project.d
@@ -0,0 +1,1 @@
+/home/linux/rust/rust_first_application/project/target/debug/project: /home/linux/rust/rust_first_application/project/src/main.rs
